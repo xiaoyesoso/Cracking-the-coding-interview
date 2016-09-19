39311e1d1005bb23bc2206e9de0e4f42f51ec055
Souljoy Zhuo <zhuoshoujie@126.com>
2016-09-20 06:54:32 +0800

    Create 1_5.java

---
+++ 1_5.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Zipper {
+    
+    //计算压缩后大小
+    public int countCompression(String str)
+    {
+        if(str == null || str.isEmpty()) return 0;
+        char last = str.charAt(0);
+        int count = 1;
+        int size = 0;
+        for(int i = 1 ; i < str.length() ;++i)
+        {
+            if(str.charAt(i) == last)
+            {
+                ++ count;
+            }
+            else
+            {
+                last = str.charAt(i);
+                size += 1 + String.valueOf(count).length();
+                count = 1;
+            }
+        }
+        return size + 1 + String.valueOf(count).length();
+    }
+    
+    public String zipString(String iniString) {
+        // write code here
+        int size =  countCompression(iniString);
+        if(size >= iniString.length()) return iniString;
+        StringBuffer mystr = new StringBuffer();
+        char last = iniString.charAt(0);
+        int count = 1;
+        for(int i = 1 ;i < iniString.length() ; ++i)
+        {
+            if(last == iniString.charAt(i))
+                ++count ;
+            else
+            {
+                mystr.append(last);
+                mystr.append(count);
+                last = iniString.charAt(i);
+                count = 1;
+            }
+        }
+        mystr.append(last);
+        mystr.append(count);
+        return mystr.toString();
+    }
+}
