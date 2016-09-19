7ea283efde002227cf12cfdd9204493cfe850138
Souljoy Zhuo <zhuoshoujie@126.com>
2016-09-20 06:58:32 +0800

    翻转子串

---
+++ 1_8.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ReverseEqual {
+    public boolean checkReverseEqual(String s1, String s2) {
+        // write code here
+        int len = s1.length();
+        if(len != 0 && len == s2.length())
+        {
+            String s1s1 = s1 + s1;
+            return s1s1.indexOf(s2) != -1;
+        }
+        return false;
+    }
+}
